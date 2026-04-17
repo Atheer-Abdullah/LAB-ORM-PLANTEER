8fb842324433bc1e932ada7f73f5218e1949d2a4
Atheer Alharthi <atheerabdullahalharthi@gmail.com>
2026-04-17 05:08:54 +0300

    feat: implement Contact Us page and define URL patterns for contact messages

--- main/views.py
+++ main/views.py
@@ -5,9 +5,9 @@ def home_view(request: HttpRequest):
     
     return render(request, "main/index.html")
 
-def contact_us_view(request: HttpRequest):
-
-    return render(request, "main/contact.html")
+def contact_us_view(request):
+    
+    return render(request, 'main/contact_us.html')
 
 def contact_messages_view(request: HttpRequest):
     
