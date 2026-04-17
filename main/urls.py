427ff540834ea4a1cf76835f6cad96756b543fa8
Atheer Alharthi <atheerabdullahalharthi@gmail.com>
2026-04-17 03:52:52 +0300

    feat: implement base template with navbar and footer structure

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,6 @@ app_name = "main"
 
 urlpatterns = [
     path("", views.home_view, name="home_view"),
+    path("contact/", views.contact_us_view, name="contact_us_view"),
+    path("contact/messages/", views.contact_messages_view, name="contact_messages_view"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
-# Create your views here.
+from django.http import HttpRequest
 
 def home_view(request: HttpRequest):
+    
     return render(request, "main/index.html")
+
+def contact_us_view(request: HttpRequest):
+
+    return render(request, "main/contact.html")
+
+def contact_messages_view(request: HttpRequest):
+    
+    return render(request, "main/messages.html")
